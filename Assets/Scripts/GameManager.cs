c2ede79fe30091fafe766cfc88e57f5f0e089808
greenreeper1 <cedricba.gautier@gmail.com>
2026-04-10 23:01:12 +0200

    feat :sparkles: add the mechanic of day shift

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Timer timer;
+    public DayState currentState;
+    public DayState previousState;
+    public SpriteRenderer playerSprite;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer.OnTimerEnd += SwitchDayState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (timer  != null)
+        {
+            timer.StopTimer();
+            timer.OnTimerEnd -= SwitchDayState;
+        }
+    }
+
+    public void SwitchDayState()
+    {
+        if (currentState == DayState.DAY || currentState == DayState.NIGHT)
+        {
+            previousState = currentState;
+            currentState = DayState.UPGRADING;
+            // TODO : Call the method to choose an upgrade
+            SwitchDayState(); // Delete this when the upgrades are made. It is just here to see the day / night shift
+        }
+        else if (currentState == DayState.UPGRADING)
+        {
+            if (previousState == DayState.DAY)
+            {
+                // The night awakes
+                currentState = DayState.NIGHT;
+                playerSprite.color = Color.white;
+                timer.StartTimer();
+                // TODO : Change Enemies and decor
+            }
+            else if (previousState == DayState.NIGHT)
+            {
+                // The day awakes
+                currentState = DayState.DAY;
+                playerSprite.color = Color.black;
+                timer.StartTimer();
+                // TODO : Change Enemies and decor
+            }
+        }
+    }
+}
